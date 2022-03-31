efa1934c0b55784b149a4a61ee5418fc4414a388
Mikaela Cárdenas <60782219+Mik43la@users.noreply.github.com>
2022-03-31 16:08:26 -0400

    Commit mikaela cardenas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,8 @@ public class Main {
         Solution solution = new Solution();
         int[] stones1 = {0,1,3,5,6,8,12,17};
         int[] stones2 = {0,1,2,3,4,8,9,11};
-        System.out.println(solution.canCross(stones));
+        System.out.println(solution.canCross(stones1));
+        System.out.println(solution.canCross(stones2));
     }
 }
 
