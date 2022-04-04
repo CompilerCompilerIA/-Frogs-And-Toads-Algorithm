2f41fb80bba7849cf7a63026ab497abac47dcb75
Rodrigo Viladegut <rodriviladegut@gmail.com>
2022-04-03 23:32:41 -0400

    Solver with stack - Rodrigo Viladegut

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 
 public class Solution {
     public String init(int n) {
@@ -9,12 +10,24 @@ public class Solution {
 
     public String solver(int n) {
         String answ = "";
+        String element;
+        Stack<String> stack = new Stack<>();
+        stack.push("S");
         for(int i = 0 ; i < n; i++){
             answ = answ + "S ";
+            element = "S ";
             for(int j = 0 ; j <= i; j++){
                 answ = answ +"H ";
-
+                if(j != 0){
+                    element = element + "H ";
+                }
             }
+            stack.push(element);
+        }
+        String st;
+        for(int i = 0 ; i < n; i++){
+            st = stack.pop();
+            answ = answ + st;
         }
         return answ;
     }
