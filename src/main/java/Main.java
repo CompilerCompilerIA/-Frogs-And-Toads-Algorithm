bd77ff1a2cf433aec4ab741224c284b6cae16234
Luciano Paniagua <erickmallea12345@gmail.com>
2022-04-03 23:21:35 -0400

    Proyect main - Luciano Paniagua

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] stones1 = {0,1,3,5,6,8,12,17};
-        int[] stones2 = {0,1,2,3,4,8,9,11};
-        System.out.println(solution.canCross(stones1));
-        System.out.println(solution.canCross(stones2));
+        System.out.println(solution.init(3));
     }
 }
-
